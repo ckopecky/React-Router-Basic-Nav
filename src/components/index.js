b26d76a8ff139b1cb2da1c8cd54551bd70168be2
ckopecky <kopecky12112@gmail.com>
2019-06-13 09:26:33 -0700

    reset project

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Home from './Home';
-import About from './About';
-import Contact from './Contact';
-import Navigation from './Navigation';
-
-
-export { Home, About, Contact, Navigation };
-
-//only purpose is to import components and to export them to App.js
