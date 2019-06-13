5bdfcd65246fffef8cea55f9964bfa88c7d764b3
ckopecky <kopecky12112@gmail.com>
2019-06-13 09:34:36 -0700

    update project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './components/Navigation';
+import "./App.css";
+
+const App = () => {
+    return (
+        <div>
+            <Navigation />
+        </div>
+    );
+};
+
+export default App;
